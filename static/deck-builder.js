242c7ef03c77981d6b4eb2acbb37e23c00bd134d
lemonstener <deyanvasilev7@gmail.com>
2021-06-09 17:30:35 -0500

    deck creator almost complete

--- static/deck-builder.js
+++ static/deck-builder.js
@@ -1,6 +1,7 @@
 const setPicker = document.querySelector('#set-picker')
 const cardPicker = document.querySelector('#card-picker')
 const deckDisplay = document.querySelector('#deck-display')
+const cardsInDeck = document.querySelector('#cards-in-deck')
 
 const BASE_URL = 'http://127.0.0.1:5000/api'
 
@@ -12,22 +13,135 @@ async function getCardsByFormat(format, playerClass) {
     const neutralCards = res.data.n
 
     for (card of classCards) {
-        const div = document.createElement('div')
-        div.classList.add('card')
-        div.style.backgroundImage = `url(${card.img})`
-        div.id = card.id
-        cardPicker.append(div)
+        cardPicker.append(createCardElement(card))
     }
     for (card of neutralCards) {
-        const div = document.createElement('div')
-        div.classList.add('card')
-        div.style.backgroundImage = `url(${card.img})`
-        div.id = card.id
-        cardPicker.append(div)
+        cardPicker.append(createCardElement(card))
     }
 }
 
-async function cardSet(cardSet) {
-    const res = await axios.get(`${BASE_URL}/cards/${cardSet}`)
-    console.log(res.data)
+function createCardElement(card) {
+    const div = document.createElement('div')
+    div.classList.add('card')
+    div.style.backgroundImage = `url(${card.img})`
+    div.setAttribute('id', card.id)
+    div.setAttribute('cost', card.cost)
+    div.setAttribute('cardSet', card.card_set)
+    div.setAttribute('name', card.name)
+    div.setAttribute('flavor', card.flavor)
+    div.setAttribute('howToGet', card.how_to_get)
+    div.setAttribute('type', card.type)
+    div.setAttribute('rarity', card.rarity)
+    div.setAttribute('artist', card.artist)
+    div.setAttribute('mechanics', card.mechanics)
+
+    if (card.type === 'min') {
+        div.setAttribute('attack', card.attack)
+        div.setAttribute('health', card.health)
+        div.setAttribute('minionType', card.minion_type)
+    } else if (card.type === 'spl') {
+        div.setAttribute('school', card.school)
+    } else if (card.type === 'wpn') {
+        div.setAttribute('attack', card.attack)
+        div.setAttribute('durabillity', card.durability)
+    } else {
+        div.setAttribute('attack', card.attack)
+        div.setAttribute('armor', card.armor)
+        div.setAttribute('health', card.health)
+    }
+
+    div.addEventListener('click', handleCard)
+
+    return div
 }
+
+const deckArr = []
+
+function handleCard(e) {
+    const index = parseInt(e.target.getAttribute('id'))
+    const cardRarity = e.target.getAttribute('rarity')
+    const data = findInDeck(index, cardRarity)
+    if (data.permission && deckArr.length !== 30) {
+        deckArr.push(index)
+
+        if (data.count === 1 && cardRarity !== 'lgnd') {
+            const row = document.querySelector(`[t-id='${index}']`)
+            const tdSymbol = document.createElement('td')
+            tdSymbol.innerText = 'x2'
+            tdSymbol.style.textAlign = 'center'
+            tdSymbol.style.color = 'gold'
+            row.append(tdSymbol)
+            console.log(row)
+            return
+        }
+
+        const cardName = e.target.getAttribute('name')
+
+        const cardSet = e.target.getAttribute('cardset')
+
+        const trCard = document.createElement('tr')
+        const tdLogo = document.createElement('td')
+        const tdName = document.createElement('td')
+
+        tdName.innerText = cardName
+        tdName.style.color = `${rarity[cardRarity].color}`
+        trCard.setAttribute('t-id', index)
+        trCard.addEventListener('click', removeCard)
+
+        tdLogo.style.backgroundImage = `url(${sets[cardSet].imgSrc})`
+        tdLogo.classList.add('logo')
+
+        if (cardRarity === 'lgnd') {
+            const tdSymbol = document.createElement('td')
+            tdSymbol.innerHTML = '<span>&#9733;</span>'
+            tdSymbol.style.textAlign = 'center'
+            tdSymbol.style.color = 'gold'
+            trCard.append(tdLogo)
+            trCard.append(tdName)
+            trCard.append(tdSymbol)
+            cardsInDeck.append(trCard)
+            return
+        }
+
+        trCard.append(tdLogo)
+        trCard.append(tdName)
+        cardsInDeck.append(trCard)
+    }
+}
+
+function findInDeck(index, rarity) {
+    console.log(index, rarity)
+    const data = {
+        count: 0,
+        permission: true
+    }
+
+
+    for (num of deckArr) {
+        if (index === num) {
+            data.count++
+                if (rarity === 'lgnd') {
+                    data.permission = false
+                } else
+            if (data.count > 1) {
+                data.permission = false
+            }
+        }
+    }
+
+    const card = document.querySelector(`[id='${index}']`)
+
+    if (rarity === 'lgnd' || data.count === 1) {
+        card.classList.add('grayscale')
+    }
+    return data
+}
+
+function removeCard(e) {
+
+    console.log(e.target)
+
+}
+
+
+getCardsByFormat('standard', 'warrior')
---
+++ static/variables.js
@@ -0,0 +1,125 @@
+rarity = {
+    free: {
+        cost: 0,
+        color: 'black'
+    },
+    comm: {
+        cost: 20,
+        color: '#EEF5DB'
+    },
+    rare: {
+        cost: 100,
+        color: '#54DEFD'
+    },
+    epic: {
+        cost: 400,
+        color: '#D67AB1'
+    },
+    lgnd: {
+        cost: 1600,
+        color: '#F4A259'
+    }
+}
+
+sets = {
+    c21: {
+        name: 'Core',
+        imgSrc: 'static/images/logos/stnd.png'
+    },
+    leg: {
+        name: 'Legacy',
+        imgSrc: '/static/images/formats/wild.png'
+    },
+    clsc: {
+        name: 'Classic',
+        imgSrc: '/static/images/formats/classic.png'
+    },
+    nax: {
+        name: 'Curse of Naxxramas',
+        imgSrc: '/static/images/logos/nax.png'
+    },
+    gvg: {
+        name: 'Goblins vs Gnomes',
+        imgSrc: '/static/images/logos/gvg.png'
+    },
+    brm: {
+        name: 'Blackrock Mountain',
+        imgSrc: '/static/images/logos/brm.png'
+    },
+    tgt: {
+        name: 'The Grand Tournament',
+        imgSrc: '/static/images/logos/tgt.png'
+    },
+    loe: {
+        name: 'The League of Explorers',
+        imgSrc: '/static/images/logos/loe.png'
+    },
+    wog: {
+        name: 'Whispers of the Old Gods',
+        imgSrc: '/static/images/logos/wog.png'
+    },
+    krzn: {
+        name: 'One Night in Karazhan',
+        imgSrc: '/static/images/logos/krzn.png'
+    },
+    msg: {
+        name: 'Mean Streets of Gadgetzan',
+        imgSrc: '/static/images/logos/msg.png'
+    },
+    goro: {
+        name: "Journey to Un'Goro",
+        imgSrc: '/static/images/logos/goro.png'
+    },
+    kft: {
+        name: 'Knights of the Frozen Throne',
+        imgSrc: '/static/images/logos/kft.png'
+    },
+    knc: {
+        name: 'Kobolds and Catacombs',
+        imgSrc: '/static/images/logos/knc.png'
+    },
+    wtch: {
+        name: 'The Witchwood',
+        imgSrc: '/static/images/logos/wtch.png'
+    },
+    bmp: {
+        name: 'The Boomsday Project',
+        imgSrc: '/static/images/logos/bmp.png'
+    },
+    rsr: {
+        name: "Rastakhan's Rumble",
+        imgSrc: '/static/images/logos/rsr.png'
+    },
+    ros: {
+        name: 'Rise of Shadows',
+        imgSrc: '/static/images/logos/ros.png'
+    },
+    sou: {
+        name: 'Saviors of Uldum',
+        imgSrc: '/static/images/logos/sou.png'
+    },
+    dod: {
+        name: 'Descent of Dragons',
+        imgSrc: '/static/images/logos/dod.png'
+    },
+    gka: {
+        name: "Galakrond's Awakening",
+        imgSrc: '/static/images/logos/gka.png'
+    },
+    aou: {
+        name: 'Ashes of Outland',
+        imgSrc: '/static/images/logos/aou.png'
+    },
+    schl: {
+        name: 'Scholomance Academy',
+        imgSrc: '/static/images/logos/schl.png'
+    },
+    mad: {
+        name: 'Madness at the Darkmoon Faire',
+        imgSrc: '/static/images/logos/mad.png'
+    },
+    bar: {
+        name: 'Forged in the Barrens',
+        imgSrc: '/static/images/logos/bar.png'
+    }
+}
