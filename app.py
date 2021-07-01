a3c7b8ac338bd7c99a5e537bb4924af94e6b49ca
lemonstener <deyanvasilev7@gmail.com>
2021-07-01 11:38:54 -0500

    keep going

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ curr_user = 'curr_user'
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI','postgresql:///forge_db') 
+app.config['SQLALCHEMY_DATABASE_URI'] = uri 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'secretcards13')
